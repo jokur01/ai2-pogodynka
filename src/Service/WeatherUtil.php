d5a56903a142208f8f56cb1283529e12a8fbe6a5
jp49283 <pjorkowski01@gmail.com>
2023-11-06 09:40:35 +0100

    lab-e

---
+++ src/Service/WeatherUtil.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Entity\Location;
+use App\Entity\Measurement;
+use App\Repository\MeasurementRepository;
+use App\Repository\LocationRepository;
+
+class WeatherUtil
+{
+
+    public function __construct(
+        private readonly LocationRepository $locationRepository,
+        private readonly MeasurementRepository $measurementRepository,
+    )
+    {
+    }
+
+    /**
+     * @return Measurement[]
+     */
+    public function getWeatherForLocation(Location $location): array
+    {
+        $measurements = $this->measurementRepository->findByLocation($location);
+        return $measurements;
+    }
+
+    /**
+     * @return Measurement[]
+     */
+    public function getWeatherForCountryAndCity(string $countryCode, string $city): array
+    {
+        $location = $this->locationRepository->findOneBy([
+            'country' => $countryCode,
+            'city' => $city,
+        ]);
+
+        $measurements = $this->getWeatherForLocation($location);
+
+        return $measurements;
+    }
+}
